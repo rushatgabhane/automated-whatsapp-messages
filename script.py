0db103726e5edb69ad6e47ba2399211aac1d57bb
Rushat Gabhane <rushatgabhane@gmail.com>
2018-05-29 21:18:56 +0300

    add comment

--- script.py
+++ script.py
@@ -10,9 +10,9 @@ name = "chat_name"
 message = " your_message "
 count = 10
 
-# read from file 
+# read from file and send message line by line
 file = open('data.txt', 'r')
-variable_storing_file_data = f.read().splitlines()
+file_data = f.read().splitlines()
 
 input('Press enter after scanning QR code')
 
@@ -21,11 +21,11 @@ user.click()
 msg_box = driver.find_element_by_class_name('_2S1VP')
 
 for i in range(count):
-    # line by line output of data from file
-    msg_box.send_keys(str(i+1) + message + variable_storing_file_data[i])
+    # Message Format: 'i your_message file data (line by line)'
+    # remove "str(i+1) , message to just send file data
+    msg_box.send_keys(str(i+1) + message + file_data[i])
     button = driver.find_element_by_class_name('_2lkdt')
     button.click()
     # interval between messages in seconds
     time.sleep(0.5)
-
 f.close()
